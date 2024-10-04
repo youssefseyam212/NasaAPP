0eef12c8fbd1acdc2c2ddc00c284294bd4656cfa
Yahya Elnahas <135860509+Yahya-ElNahas@users.noreply.github.com>
2024-10-04 16:02:18 +0300

    u

---
+++ Frontend/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import Cookies from 'js-cookie';
+import '../styles/home-style.css'
+
+export default function Home() {
+    const [planets, setPlanets] = useState([]);
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="home-body">
+        </div>
+    )
+}
