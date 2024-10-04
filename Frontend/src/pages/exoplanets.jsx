c4f5ceed7263cb30e11b41baeaaa7f09c1a4026b
Yahya Elnahas <135860509+Yahya-ElNahas@users.noreply.github.com>
2024-10-04 15:32:52 +0300

    u

--- Frontend/src/pages/exoplanets.jsx
+++ Frontend/src/pages/exoplanets.jsx
@@ -1,3 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import Cookies from 'js-cookie';
+import '../styles/home-style.css'
+
+export default function Exoplanets() {
+    const [planets, setPlanets] = useState([]);
+
+    const navigate = useNavigate();
 
 
-
+}
